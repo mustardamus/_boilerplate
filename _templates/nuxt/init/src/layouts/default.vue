fb76357d07b37124f8e254f36f2b994b506cf2a8
Sebastian Senf <contact@mustardamus.com>
2019-02-21 22:05:54 +0100

    also stick footer to the bottom on mobile, removed unecessary mobile breakpoint

--- _templates/nuxt/init/src/layouts/default.vue
+++ _templates/nuxt/init/src/layouts/default.vue
@@ -4,7 +4,7 @@
       <the-top-nav />
     </header>
 
-    <nuxt />
+    <nuxt id="content" />
 
     <footer>
       <the-footer />
@@ -24,11 +24,35 @@ export default {
 <style lang="scss" scoped>
 // https://philipwalton.github.io/solved-by-flexbox/demos/holy-grail/
 
+@include tablet {
+  header {
+    position: fixed;
+  }
+
+  #content {
+    margin-top: $header-top-margin-mobile;
+  }
+}
+
+@include desktop {
+  header {
+    position: relative;
+  }
+
+  #content {
+    margin-top: 0;
+  }
+}
+
 .wrapper {
   display: flex;
   min-height: 100vh;
   flex-direction: column;
   position: relative;
+}
+
+header {
+  width: 100%;
 }
 
 footer {
