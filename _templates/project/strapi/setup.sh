a193b2a3dd7a21962e23a71dc282a034386490f3
Sebastian Senf <contact@mustardamus.com>
2019-03-06 23:53:33 +0100

    reverted prompts for strapi db stuff

--- _templates/project/strapi/setup.sh
+++ _templates/project/strapi/setup.sh
@@ -1,3 +1,3 @@
 ---
-sh: "npx hygen eslint node && npx hygen eslint jest && npx hygen jest init && npx hygen strapi init --dbName changeme --dbUser changeme --dbPass changeme"
+sh: "npx hygen eslint node && npx hygen eslint jest && npx hygen jest init && npx hygen strapi init"
 ---
