4a497b6f6c14c0c3d5f0d9dd769413bd6e293412
Sebastian Senf <contact@mustardamus.com>
2019-03-06 23:42:29 +0100

    set variables for strapi project template

--- _templates/project/strapi/setup.sh
+++ _templates/project/strapi/setup.sh
@@ -1,3 +1,3 @@
 ---
-sh: "npx hygen eslint node && npx hygen eslint jest && npx hygen jest init && npx hygen strapi init"
+sh: "npx hygen eslint node && npx hygen eslint jest && npx hygen jest init && npx hygen strapi init --dbName changeme --dbUser changeme --dbPass changeme"
 ---
